3b23cf485c4b5057957c88ab7f782104e446cac8
PrafulBangar <prafulbangar581@gmail.com>
2019-07-15 10:41:49 +0530

    test case run for value class

--- src/test/scala/ValueClassTest.scala
+++ src/test/scala/ValueClassTest.scala
@@ -7,12 +7,12 @@ class ValueClassTest extends  WordSpec {
   val firstNameObject=new FirstName("Praful")
   val lastNameObject=new LastName("Bangar")
   val ageObject=new Age(21)
-
+val result=println(firstNameObject+ " " + lastNameObject + " is of " + ageObject + " years")
  val result1=displayDetails.displayDetails(firstNameObject,lastNameObject,ageObject)
-
+//result1.equals(result1)
   "Output requred" should {
     "Firstname+lastName+Age" in {
-      assert("Praful is of 21 years"== result1)
+      assert(result.equals(result1))
     }
   }
 }
